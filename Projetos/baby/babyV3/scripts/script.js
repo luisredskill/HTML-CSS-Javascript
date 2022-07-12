9fb6bee4f783e89975613fa59f7d46f4492cc4ea
Luis Carlos Barberino Ribeiro <jklcw71@gmail.com>
2022-07-12 09:00:10 -0300

    Version 3

---
+++ Projetos/baby/babyV3/scripts/script.js
@@ -0,0 +1,37 @@
+function copy() {
+    var copyText = document.getElementById("librarycode");
+    navigator.clipboard.writeText(copyText.innerText);
+  }
+
+function senha(){
+  var senha = document.getElementById('senha').value;
+
+  if (senha == "steven universe") {
+    window.location.href = "paginas/pag02.html";
+  } else {};
+}
+
+function senha2(){
+  var senha = document.getElementById('senha').value;
+
+  if (senha == "eu te amo muito muito") {
+    window.location.href = "Final.html";
+  } else {};
+}
+
+function pularpag(){
+
+}
+
+function pag03(){
+  window.location.href = "pag03.html";
+}
+
+function pag04(){
+  window.location.href = "pag04.html";
+}
+
+function comovocesesente(){
+  window.location.href = "Senha-eu_te_amo_muito_muito.html";
+}
+
