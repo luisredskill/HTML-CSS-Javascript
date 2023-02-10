1eefff21140faf87c5a6f72324ada6b3927da6de
Luis Carlos Barberino Ribeiro <jklcw71@gmail.com>
2023-02-10 16:22:38 -0300

    Rewatch--ReactCourse-2Hours

---
+++ Projetos/React-learning/React-projects/Guided_studies_2/01_react_facts/src/Components/App.jsx
@@ -0,0 +1,14 @@
+import '../CSS/App.css';
+import Main from './Main';
+import Nav from './Nav'
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ Projetos/React-learning/React-projects/Guided_studies_2/01_react_facts/src/Components/Nav.jsx
@@ -0,0 +1,9 @@
+import logo from "../Img/logo192.png"
+
+export default function Nav() {
+    return (
+        <nav>
+            <img src={logo} width="40px" alt="" />
+        </nav>
+    )
+}
